662008e228169a5dbbc8d0b34283ee7155c12425
FILÂTRE Killian <k.filatre@gmail.com>
2025-10-23 23:44:31 +0200

    setup workflows for test and coverage

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
+        { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
       ]
     },
